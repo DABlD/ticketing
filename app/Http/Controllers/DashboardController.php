3dd16a995fc45fff8cbc648cf74c07da0e902c3e
David Mendoza <davidmendozaofficial@gmail.com>
2023-09-03 03:50:58 +0800

    dsahboard charts part 1
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{Event, Ticket, Transaction};
 
 class DashboardController extends Controller
 {
     function index(){
+        $te = Event::all();
+        $tc = Event::where('status', 'Finished')->get();
+        $tu = Event::where('status', 'Upcoming')->geT();
+        $ttp = Transaction::whereIn('status', ["Paid", "Used"])->get();
+        $ttu = Transaction::where('status', 'Unpaid')->get();
+
         return $this->_view('dashboard', [
-            'title'         => 'Dashboard'
+            'title'         => 'Dashboard',
+            'te'            => $te,
+            'tc'            => $tc,
+            'tu'            => $tu,
+            'ttp'           => $ttp,
+            'ttu'           => $ttu
         ]);
     }
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,16 +3,18 @@
 
 <section class="content">
     <div class="container-fluid">
+
+        {{-- CARD --}}
         <div class="row">
             <div class="col-lg-3 col-6">
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
-                        <p>Total Events</p>
+                        <h3>{{ $te->count() }}</h3>
+                        <p>Total List</p>
                     </div>
 
                     <div class="icon">
-                        <i class="ion ion-bag"></i>
+                        <i class="ion ion-calendar"></i>
                     </div>
                 </div>
             </div>
@@ -20,11 +22,11 @@
             <div class="col-lg-3 col-6">
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
-                        <p>Completed Events</p>
+                        <h3>{{ $tc->count() }}</h3>
+                        <p>Total Completed Events</p>
                     </div>
                     <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
+                        <i class="ion ion-checkmark"></i>
                     </div>
                 </div>
             </div>
@@ -32,7 +34,7 @@
             <div class="col-lg-3 col-6">
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
+                        <h3>{{ $tu->count() }}</h3>
                         <p>Upcoming Events</p>
                     </div>
 
@@ -45,11 +47,11 @@
             <div class="col-lg-3 col-6">
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
-                        <p>Total Tickets Sold</p>
+                        <h3>{{ $ttp->count() }} / {{ $ttu->count() }}</h3>
+                        <p>Total Tickets Paid / Unpaid</p>
                     </div>
                     <div class="icon">
-                        <i class="ion ion-stats-bars"></i>
+                        <i class="ion ion-cash"></i>
                     </div>
                 </div>
             </div>
@@ -67,10 +69,13 @@
                         <div class="card-tools">
                             <ul class="nav nav-pills ml-auto">
                                 <li class="nav-item">
-                                    <a class="nav-link active" href="#tab1" data-toggle="tab">Tab 1</a>
+                                    <a class="nav-link active" href="#tab1" data-toggle="tab">Today</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#tab2" data-toggle="tab">Tab 2</a>
+                                    <a class="nav-link" href="#tab2" data-toggle="tab">7 Days</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#tab3" data-toggle="tab">30 Days</a>
                                 </li>
                             </ul>
                         </div>
@@ -78,11 +83,14 @@
 
                     <div class="card-body">
                         <div class="tab-content p-0">
-                            <div class="chart tab-pane active" id="tab1" style="position: relative; height: 300px;">
-                                TAB 1
+                            <div class="chart tab-pane active" id="tab1" style="position: relative; height: 100%;">
+                                <canvas id="report1" width="100%" height="100%"></canvas>
                             </div>
-                            <div class="chart tab-pane" id="tab2" style="position: relative; height: 300px;">
-                                TAB 2
+                            <div class="chart tab-pane" id="tab2" style="position: relative; height: 100%;">
+                                <canvas id="report2" width="100%" height="100%"></canvas>
+                            </div>
+                            <div class="chart tab-pane" id="tab3" style="position: relative; height: 100%;">
+                                <canvas id="report3" width="100%" height="100%"></canvas>
                             </div>
                         </div>
                     </div>
@@ -91,5 +99,56 @@
         </div>
     </div>
 </section>
+
+@push('styles')
+@endpush
+
+@push('scripts')
+    <script src="{{ asset('js/charts.min.js') }}"></script>
+    <script src="{{ asset('js/charts-utils.min.js') }}"></script>
+    <script>
+        $(document).ready(() => {
+            new Chart(document.getElementById("report1"), {
+              type: 'line',
+              data: {
+                labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+                datasets: [{ 
+                    data: [86,114,106,106,107,111,133,221,783,2478],
+                    label: "Africa",
+                    borderColor: "#3e95cd",
+                    fill: false
+                  }, { 
+                    data: [282,350,411,502,635,809,947,1402,3700,5267],
+                    label: "Asia",
+                    borderColor: "#8e5ea2",
+                    fill: false
+                  }, { 
+                    data: [168,170,178,190,203,276,408,547,675,734],
+                    label: "Europe",
+                    borderColor: "#3cba9f",
+                    fill: false
+                  }, { 
+                    data: [40,20,10,16,24,38,74,167,508,784],
+                    label: "Latin America",
+                    borderColor: "#e8c3b9",
+                    fill: false
+                  }, { 
+                    data: [6,3,2,2,7,26,82,172,312,433],
+                    label: "North America",
+                    borderColor: "#c45850",
+                    fill: false
+                  }
+                ]
+              },
+              options: {
+                title: {
+                  display: true,
+                  text: 'World population per region (in millions)'
+                }
+              }
+            });
+        });
+    </script>
+@endpush
 
 @endsection

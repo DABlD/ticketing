1c27de14a7953fa8126b2a0702722b27748c7dc6
David Mendoza <davidmendozaofficial@gmail.com>
2023-11-24 21:50:41 +0800

    test
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -6,6 +6,8 @@
 		{{-- <img src="data:image/png;base64, {{ base64_encode(QrCode::size(300)->format('png')->generate(route("api.verify") . '?crypt=' . $data->crypt)) }} "> --}}
 		<img height="100" width="100" src="data:image/png;base64, {{ base64_encode(QrCode::format('png')->size(100)->generate('Make me into an QrCode!')) }} ">
 
+		data:image/png;base64, {{ base64_encode(QrCode::format('png')->size(100)->generate('Make me into an QrCode!')) }}
+
 		<br>
         Event: {{ $data->ticket->event->name }}
         <br>
